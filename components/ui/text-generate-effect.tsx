a86d13d1f908e8a42dc4ae0d30438efd80736099
AMBUJ SINGH <82210881+Ambuj1264@users.noreply.github.com>
2024-08-03 20:56:37 +0000

    Add .env to .gitignore and remove it from tracking

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -15,7 +15,7 @@ export const TextGenerateEffect = ({
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  let wordsArray = words?.split(" ");
   useEffect(() => {
     animate(
       "span",
